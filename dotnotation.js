581f213958bd8dd6bf216937d6c22ad2c6df03e4
Daniel Worthington <daniel.worthington96@outlook.com>
2024-08-05 13:33:03 +0100

    word print

---
+++ dotnotation.js
@@ -0,0 +1,15 @@
+console.log("Hello World!")
+
+/*
+console.log(math.floor(Math.random()*10))
+*/
+
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("---------")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("---------")
+console.log("  |   |  ")
+console.log("  |   |  ")
+
