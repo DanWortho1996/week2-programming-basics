de5a180ad775175eb9fa176c70405f675a05d957
Daniel Worthington <daniel.worthington96@outlook.com>
2024-08-06 16:00:17 +0100

    ifelse Updated

--- ifelse.js
+++ ifelse.js
@@ -7,32 +7,117 @@
     //if nothing met this will show
 //}
 
-let weather = "sunny";
+// let weather = "sunny";
 
-if (weather == "sunny"){
-    console.log("Well, I better wear some E45 init")
-}else if (weather == "rainy"){
-    console.log("Go Get the umbrella lad")
-} else{
-    console.log("Hmmm. Should of got Factor 50 instead.")
-};
+// if (weather == "sunny"){
+//     console.log("Well, I better wear some E45 init")
+// }else if (weather == "rainy"){
+//     console.log("Go Get the umbrella lad")
+// } else{
+//     console.log("Hmmm. Should of got Factor 50 instead.")
+// };
 
-let dice = Math.floor(Math.random()*9);
-//Mini game with random number generator shown above
-if (dice > 6){
-    console.log(`You Win! you rolled ${dice}`)
+// let dice = Math.floor(Math.random()*9);
+// //Mini game with random number generator shown above
+// if (dice > 6){
+//     console.log(`You Win! You rolled ${dice}`)
+// }else{
+//         console.log("You Loose")
+    // }
+
+// let place = "London"
+// let weathers = "Cloudy"
+
+// if (place == "London" &&  weathers == "Cloudy"){
+//     console.log("Check Again")
+// }else if (place == "London" && weathers == "Cloudy"){
+//     console.log("Obviously")
+// }else {
+//     console.log("What? It isn't cloudy for once?")
+// }
+
+//let dicey = 4;
+
+// switch(dicey){
+//     case 1:
+//         console.log("Roll Again")
+//         break;
+//     case 2:
+//         console.log("Roll Again")
+//         break;
+//     case 3:
+//         console.log("Roll Again")
+//         break;
+//     case 4:
+//         console.log("You Finally Win!")
+// }
+
+
+//Activity 1
+let country = "UK"
+let age = 18;
+if (age > 17 && country == "UK"){
+    console.log(`Yes I can serve you even though it is illegal`)
 }else{
-        console.log("You Loose")
+        console.log("You aren't old enough, get moving")
     }
 
-let place = "London"
-let weathers = "Cloudy"
-
-if (place == "London" &&  weathers == "Cloudy"){
-    console.log("Check Again")
-}else if (place == "London" && weathers == "Cloudy"){
-    console.log("Obviously")
-}else {
-    console.log("What? It isn't cloudy for once?")
+//Activity 2
+let topping = "Pepperoni"
+let topping1 = "Chicken"
+let topping2 = "Beef"
+switch(topping, topping1, topping2){
+    case topping:
+        console.log("I forgot my pepperoni")
+        break;
+    case topping1:
+        console.log("Don't forget the Chicken!")
+        break;
+    case topping2:
+        console.log(`These are important ingredients for my Pizza, ${topping}, ${topping1}, ${topping2}. This is a Meat Feast!`)
 }
 
+//Activity 3
+let password = Math.floor(Math.random()*16);
+if (password < 8){
+    console.log("The password must be 8 characters long")
+}else{
+    console.log("Success Password, You will now be logged in, Please Wait...")
+
+    }
+
+//Activity 4
+let num  = 15;
+if (num / 3){
+    console.log("Fizz")
+}else if (num / 5){
+    console.log("Buzz")
+}else{
+    (num / 3 && 5)
+    console.log("This is Fizz & Buzz")}
+
+//Activity 5
+let numb = 10101;
+let numb1 = 20202;
+if(numb == 10101 && numb1 == 20202){
+    console.log(numb)
+}else(numb = 20202)
+    console.log("20202")
+
+//Activity 6
+let placeOfWork = 7;
+let time = 8;
+let townOfHome = 9;
+if (placeOfWork == 7){
+    console.log(`I'm stuck at work until ${placeOfWork}`)
+}if(time == 8){
+    console.log(`I'm socializing with friends until about ${time} on the Xbox`)
+}else(townOfHome == 9)
+    console.log(`I am at home until ${townOfHome}`)
+
+//Activity 7
+let blahh = "i"
+//Last vowel was an "i"
+console.log(blahh)
+
+//Activity 8
